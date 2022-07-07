162d6bc3c5a2cd0d6199ad33bf4fe98085962871
AnupYadav121 <anup.yadav@razorpay.com>
2022-07-07 23:24:24 +0530

    final code changes suggested  added

--- 6thJulyQuestion3.go
+++ 6thJulyQuestion3.go
@@ -4,58 +4,71 @@ import (
 	"errors"
 	"fmt"
 	"sync"
-	"time"
 )
 
 type Account struct {
-	mu       sync.Mutex
-	balances map[string]int
+	mu      sync.Mutex
+	id      int
+	balance int
+	name    string
 }
 
-func (c *Account) add(name string, rupee int) {
-	c.mu.Lock()
-	c.balances[name] += rupee
-	c.mu.Unlock()
+type Bank struct {
+	accounts []Account
 }
 
-func (c *Account) subtract(name string, rupee int) {
-	c.mu.Lock()
-	c.balances[name] -= rupee
-	c.mu.Unlock()
+func (b *Bank) add(rupee int, id int) {
+	b.accounts[id].mu.Lock()
+	b.accounts[id].balance += rupee
+	b.accounts[id].mu.Unlock()
+}
+
+func (b *Bank) subtract(rupee int, id int) {
+	b.accounts[id].mu.Lock()
+	b.accounts[id].balance -= rupee
+	b.accounts[id].mu.Unlock()
 }
 
 func main() {
-	c := Account{
-		balances: map[string]int{"anup": 0, "kashi": 0},
+	account1 := Account{
+		balance: 0,
+		name:    "anup",
+		id:      0,
+	}
+
+	account2 := Account{
+		balance: 0,
+		name:    "kashi",
+		id:      1,
+	}
+
+	b := Bank{
+		[]Account{account1, account2},
 	}
 
 	var wg sync.WaitGroup
 
-	doDeposit := func(name string, n int) {
-
-		c.add(name, n)
+	doDeposit := func(name string, n int, id int) {
+		b.add(n, id)
 		wg.Done()
 	}
 
-	doWithdraw := func(name string, n int) {
-		if c.balances[name] < n {
-			fmt.Println(nil, errors.New("Sorry , can not withdraw more than current balance amount"))
+	doWithdraw := func(name string, n int, id int) {
+		if b.accounts[id].balance < n {
+			fmt.Println(errors.New("Sorry , can not withdraw more than current balance amount"))
 			wg.Done()
 		} else {
-			c.subtract(name, n)
+			b.subtract(n, id)
 			wg.Done()
 		}
 	}
 
-	fmt.Println(c.balances)
-
 	wg.Add(4)
-	go doDeposit("anup", 10000)
-	go doDeposit("kashi", 10000)
-	time.Sleep(time.Second)
-	go doWithdraw("anup", 10001)
-	go doWithdraw("kashi", 10001)
+	go doDeposit("anup", 10000, 0)
+	go doDeposit("kashi", 10000, 1)
+	go doWithdraw("anup", 100, 0)
+	go doWithdraw("kashi", 1001, 1)
 
 	wg.Wait()
-	fmt.Println(c.balances)
+	fmt.Println(b.accounts)
 }
